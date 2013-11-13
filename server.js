45afe231bc104a8a3da1cf9226b3cbe7f8d4046b
OpenShift guest <pturchinetz@hotmail.com>
2013-11-12 20:30:41 -0500

    hi

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var whatislife = {
+"name" : "Paul Turchinetz",
+"vacation" : {
+        "places": ["59.93, 30.36","19.38, 11.38","38.94, -111.45"]
+        }
+};
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.json(whatislife);
 });
 
 var port = process.env.PORT || 5000;
